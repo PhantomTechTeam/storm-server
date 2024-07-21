f38a588c6a728eca883c7ac12adbf1dc2d139295
zenith110 <thequake@live.com>
2024-07-20 20:33:04 -0400

    Updated parsing of origin on backend

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = environ.get("ORIGINS")
+origins = str(environ.get("ORIGINS")).split(",")
 
 app.add_middleware(
     CORSMiddleware,

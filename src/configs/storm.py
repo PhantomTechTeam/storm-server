4be15e6fdcb3eaa95d349e31eb418ceb1c0bd3cd
zenith110 <thequake@live.com>
2024-08-03 10:53:19 -0400

    Updated code to test directory in railway

--- src/configs/storm.py
+++ src/configs/storm.py
@@ -3,7 +3,7 @@ from knowledge_storm import STORMWikiRunnerArguments, STORMWikiRunner, STORMWiki
 # For a good practice, choose a cheaper/faster model for `conv_simulator_lm` which is used to split queries, synthesize answers in the conversation. Choose a more powerful model for `article_gen_lm` to generate verifiable text with citations.
 lm_configs = STORMWikiLMConfigs()
 
-def setup_storm_engine_args(args: dict[str, str], topic: str):
+def setup_storm_engine_args(args: dict[str, str]):
     output_dir_original = args.get("output_dir")
     
     # Check out the STORMWikiRunnerArguments class for more configurations.
@@ -19,7 +19,7 @@ def setup_storm_engine_args(args: dict[str, str], topic: str):
 def run_runner(topic: str, engine_args, rm):
     runner = STORMWikiRunner(engine_args, lm_configs, rm)
     runner.run(
-            topic=topic.lower(),
+            topic=topic,
             do_research=True,
             do_generate_outline=True,
             do_generate_article=True,

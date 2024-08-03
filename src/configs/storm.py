22ac066eb378cf66aeb82b50449eb51b0fb7a1be
Abrahan N. <35606009+zenith110@users.noreply.github.com>
2024-08-03 19:59:37 -0400

    Users/zenith110/debugging prod (#11)
    * Updated code to test directory in railway
    
    * Updated code for engine_args
    
    * Fixed listdir issue
    
    * Updated current folder
    
    * Updated folder directory
    
    * Updated output dir
    
    * Fixed issue with articles

--- src/configs/storm.py
+++ src/configs/storm.py
@@ -3,12 +3,12 @@ from knowledge_storm import STORMWikiRunnerArguments, STORMWikiRunner, STORMWiki
 # For a good practice, choose a cheaper/faster model for `conv_simulator_lm` which is used to split queries, synthesize answers in the conversation. Choose a more powerful model for `article_gen_lm` to generate verifiable text with citations.
 lm_configs = STORMWikiLMConfigs()
 
-def setup_storm_engine_args(args: dict[str, str], topic: str):
+def setup_storm_engine_args(args: dict[str, str]):
     output_dir_original = args.get("output_dir")
     
     # Check out the STORMWikiRunnerArguments class for more configurations.
     engine_args = STORMWikiRunnerArguments(
-        output_dir=output_dir_original,
+        output_dir=f"{output_dir_original}",
                 max_conv_turn=int(str(args.get("max_conv_turn"))),
                 max_perspective=int(str(args.get("max_perspective"), )),
                 search_top_k=int(str(args.get("search_top_k"))),
@@ -19,7 +19,7 @@ def setup_storm_engine_args(args: dict[str, str], topic: str):
 def run_runner(topic: str, engine_args, rm):
     runner = STORMWikiRunner(engine_args, lm_configs, rm)
     runner.run(
-            topic=topic.lower(),
+            topic=topic,
             do_research=True,
             do_generate_outline=True,
             do_generate_article=True,
--- src/routes/create_article.py
+++ src/routes/create_article.py
@@ -1,15 +1,23 @@
 from configs import openai, storm
+import os
 def create_article(topic: str):
+    updated_topic = topic.replace(" ", "_").lower()
     open_ai_kwargs = openai.setup_openai_kwargs()
     openai.setup_gpt_3_5(open_ai_kwargs)
     print("Successfully set up openai!")
     from configs import argparser, you_com, file_parser
-    engine_args = storm.setup_storm_engine_args(argparser.parser, topic)
+    engine_args = storm.setup_storm_engine_args(argparser.parser)
     print("Successfully retrieved engine_args!\n")
     rm = you_com.setup_you_rm(engine_args)
     print("Successfully got rm from you.com!\nRunning runner now!\n")
-    storm.run_runner(topic, engine_args, rm)
+    storm.run_runner(updated_topic, engine_args, rm)
     print("Finished running runner!\nParsing file now to send to frontend!\n")
-    json_contents = file_parser.parse_file(topic)
+    print(f"All folders are {os.getcwd()}")
+    
+    
+    current_folder = os.listdir(f'{os.getenv("OUTPUT_DIR")}/{updated_topic}')
+    for file in current_folder:
+        print(f"files are {file}")
+    json_contents = file_parser.parse_file(updated_topic)
     print("Fully parsed file, sending response back!\n")
     return json_contents

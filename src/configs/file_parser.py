056f3f977e1fc79cf70f84ea623d33dbd9bb5594
zenith110 <thequake@live.com>
2024-08-04 20:48:53 -0400

    Updated styling of file parser

--- src/configs/file_parser.py
+++ src/configs/file_parser.py
@@ -9,13 +9,19 @@ def parse_file(topic: str):
     updated_topic = topic.replace(" ", "_").lower()
     output_dir = environ.get("OUTPUT_DIR")
     folder_path = f"{output_dir}/{updated_topic}"
+
     for file in listdir(folder_path):
         print(f"file is {file}")
+
     with open(f"{folder_path}/storm_gen_article_polished.txt", "r+") as f:
         content.append(str(f.read()))
+
     escapable_str_with_comma = '","'
+
     escapable_str_without_comma = '"'
+
     double_escapes = "\n\n"
+
     final_results = (
         "".join(content)
         .replace(escapable_str_with_comma, "")
@@ -25,6 +31,7 @@ def parse_file(topic: str):
 
     with open(f"{folder_path}/url_to_info.json", "r+") as f:
         data = json.load(f)
+
     url_unified_index = data.get("url_to_unified_index")
 
     urls_to_unified_indexes = {}
@@ -32,6 +39,7 @@ def parse_file(topic: str):
     for url, unified_index in url_unified_index.items():
         if unified_index not in urls_to_unified_indexes.keys():
             urls_to_unified_indexes[unified_index] = url
+    
     result = {
         "content": final_results,
         "urls_to_unified_index": OrderedDict(sorted(urls_to_unified_indexes.items())),

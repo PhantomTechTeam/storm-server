d0737dd8b7e96d839577b39e355d46d9c31edb8c
zenith110 <thequake@live.com>
2024-08-04 20:30:33 -0400

    Updated ollama code with formatting

--- src/configs/ollama.py
+++ src/configs/ollama.py
@@ -1,7 +1,7 @@
 import os
 from knowledge_storm import OllamaClient
 class OllamaConfig: 
-    def __init__(self):
+    def __init__(self, lm_configs):
         self.model = os.environ.get("OLLAMA_MODEL", "llama3:8b")
         self.port = os.environ.get("OLLAMA_PORT", 11434)
         self.url = os.environ.get("OLLAMA_URL", "http://localhost")
@@ -16,3 +16,9 @@ class OllamaConfig:
         outline_gen_lm = OllamaClient(max_tokens=400, **self.ollama_kwargs)
         article_gen_lm = OllamaClient(max_tokens=700, **self.ollama_kwargs)
         article_polish_lm = OllamaClient(max_tokens=4000, **self.ollama_kwargs)
+
+        lm_configs.set_conv_simulator_lm(conv_simulator_lm)
+        lm_configs.set_question_asker_lm(question_asker_lm)
+        lm_configs.set_outline_gen_lm(outline_gen_lm)
+        lm_configs.set_article_gen_lm(article_gen_lm)
+        lm_configs.set_article_polish_lm(article_polish_lm)
